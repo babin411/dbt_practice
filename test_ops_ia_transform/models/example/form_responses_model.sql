f3a4f154d695a93b656a57d4cfedbdb0ce0b9e6f
babin411 <j.babin411@gmail.com>
2023-03-02 22:56:54 +0545

    trying_dbt_custom_transformation

---
+++ test_ops_ia_transform/models/example/form_responses_model.sql
@@ -0,0 +1,30 @@
+--  create  table "postgres".quarantine."covid_epidemiology_f11__dbt_tmp"
+--   as (
+
+with raw_form_responses as (
+-- SQL model to parse JSON blob stored in a single column and extract into separated field columns as described by the JSON Schema
+select
+    jsonb_extract_path_text(_airbyte_data, 'Day') as Day,
+    jsonb_extract_path_text(_airbyte_data, 'Email') as email,
+    jsonb_extract_path_text(_airbyte_data, 'Shift') as shift,
+    jsonb_extract_path_text(_airbyte_data, 'Timestamp') as Timestamp,
+    jsonb_extract_path_text(_airbyte_data, 'Employee_Name') as employee_name,
+    jsonb_extract_path_text(_airbyte_data, 'Food_Coupon_ID') as food_coupon_id,
+    jsonb_extract_path_text(_airbyte_data, 'Sub_Department') as sub_department,
+    jsonb_extract_path_text(_airbyte_data, 'Main_Department') as main_department,
+    jsonb_extract_path_text(_airbyte_data, 'Drop_Off_Required') as drop_off_required
+    _airbyte_emitted_at
+from "raw".raw_data._airbyte_raw_test_form_reponses
+-- covid_epidemiology
+)
+select
+    Day,
+    email,
+    shift,
+    Timestamp,
+    UPPER(employee_name),
+    food_coupon_id,
+    sub_department,
+    main_department,
+    drop_off_required
+from raw_form_respones 
